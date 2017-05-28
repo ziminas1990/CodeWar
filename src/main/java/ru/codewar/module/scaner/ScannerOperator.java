1d0590fa2eaec89e3ce9505dfaf5b9a4af58b601
Zimin AS <ziminas1990@gmail.com>
2017-05-28 12:31:10 +0300

    Added ScannerOperator

--- src/main/java/ru/codewar/module/scaner/ScannerOperator.java
+++ src/main/java/ru/codewar/module/scaner/ScannerOperator.java
@@ -1,10 +1,20 @@
 package ru.codewar.module.scaner;
 
+import ru.codewar.networking.Message;
 import ru.codewar.protocol.module.ModuleOperator;
 import ru.codewar.world.CelestialBody;
 
 import java.util.ArrayList;
 
 public class ScannerOperator extends ModuleOperator {
-    void onScanningComplete(ArrayList<CelestialBody> bodies) {}
+
+    public void onScanningComplete(int transactionId, ArrayList<CelestialBody> bodies) {
+        // information about bodies will be represented as JSON array
+        StringBuilder response = new StringBuilder();
+        response.append("scanned {");
+        for(CelestialBody body : bodies)
+            response.append(body.toJson());
+        response.append("}");
+        onResponse(transactionId, new Message(response.toString()));
+    }
 }
